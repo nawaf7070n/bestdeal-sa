296bffa4138e3861431a574cdac7d141870ac969
nawaf7070n <nawaf7070n@gmail.com>
2026-09-08 19:16:38 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: { primary: "#FFD700", dark: "#0f172a" }
+    }
+  },
+  plugins: []
+}
